dd8791fd0e68b8f6f276f08d43e838b8e0655795
Geovana Possenti <geovana.possenti@ifood.com.br>
2021-04-20 16:34:26 -0300

    Using struct to store account data

--- main.go
+++ main.go
@@ -2,18 +2,20 @@ package main
 
 import "fmt"
 
+type ContaCorrente struct {
+	titular       string
+	numeroAgencia int
+	numeroConta   int
+	saldo         float64
+}
+
 func main() {
-	var titular string = "Geovana"
-	var numeroAgencia int = 589
-	var numeroConta int = 123456
-	var saldo float64 = 125.50
+	contaDaGeovana := ContaCorrente{titular: "Geovana",
+		saldo: 125.5}
 
-	fmt.Println(titular, numeroAgencia, numeroConta, saldo)
+	contaDaBruna := ContaCorrente{"Bruna", 222, 111222, 200}
 
-	var titular2 string = "Luciene"
-	var numeroAgencia2 int = 555
-	var numeroConta2 int = 111333
-	var saldo2 float64 = 200
-	fmt.Println(titular2, numeroAgencia2, numeroConta2, saldo2)
+	fmt.Println(contaDaGeovana)
+	fmt.Println(contaDaBruna)
 
 }
