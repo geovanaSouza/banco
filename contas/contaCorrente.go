8550a3c8bc03c8a610a422211fbbc9f35c170e05
Geovana Possenti <geovana.possenti@ifood.com.br>
2021-04-21 16:53:04 -0300

    Usando composição para titular, tornando saldo privado e criando função para retornar saldo

--- contas/contaCorrente.go
+++ contas/contaCorrente.go
@@ -1,50 +1,58 @@
 package contas
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/geovanaSouza/banco/clientes"
+)
 
 type ContaCorrente struct {
-	Titular       string
+	Titular       clientes.Titular
 	NumeroAgencia int
 	NumeroConta   int
-	Saldo         float64
+	saldo         float64
 }
 
 func (c *ContaCorrente) Sacar(valorDoSaque float64) string {
-	fmt.Println("Saldo da conta:", c.Saldo)
+	fmt.Println("saldo da conta:", c.saldo)
 	fmt.Println("Sacando", valorDoSaque, "da conta")
-	podeSacar := valorDoSaque > 0 && valorDoSaque <= c.Saldo
+	podeSacar := valorDoSaque > 0 && valorDoSaque <= c.saldo
 	if podeSacar {
-		c.Saldo -= valorDoSaque
-		fmt.Println("Novo valor de saldo da conta:", c.Saldo)
+		c.saldo -= valorDoSaque
+		fmt.Println("Novo valor de saldo da conta:", c.saldo)
 		return "Saque relizado com sucesso\n"
 	} else if valorDoSaque < 0 {
 		return "Não é possível fazer saque negativo\n"
 	} else {
-		return "Saldo insuficiente\n"
+		return "saldo insuficiente\n"
 	}
 }
 
 func (c *ContaCorrente) Depositar(valorDoDeposito float64) (string, float64) {
-	fmt.Println("Saldo da conta:", c.Saldo)
+	fmt.Println("saldo da conta:", c.saldo)
 	fmt.Println("Depositando", valorDoDeposito, "na conta")
 	if valorDoDeposito > 0 {
-		c.Saldo += valorDoDeposito
-		return "Deposito realizado com sucesso", c.Saldo
+		c.saldo += valorDoDeposito
+		return "Deposito realizado com sucesso", c.saldo
 	} else {
-		return "Valor do deposito menor que zero", c.Saldo
+		return "Valor do deposito menor que zero", c.saldo
 	}
 }
 
 func (c *ContaCorrente) Transferir(valorDaTransferencia float64, contaDestino *ContaCorrente) bool {
-	fmt.Println("Saldo da conta Origem:", c.Saldo)
-	fmt.Println("Saldo da conta Destino:", contaDestino.Saldo)
-	if valorDaTransferencia < c.Saldo && valorDaTransferencia > 0 {
-		c.Saldo -= valorDaTransferencia
+	fmt.Println("saldo da conta Origem:", c.saldo)
+	fmt.Println("saldo da conta Destino:", contaDestino.saldo)
+	if valorDaTransferencia < c.saldo && valorDaTransferencia > 0 {
+		c.saldo -= valorDaTransferencia
 		contaDestino.Depositar(valorDaTransferencia)
-		fmt.Println("Novo saldo da conta Origem:", c.Saldo)
-		fmt.Println("Novo saldo da conta Destino:", contaDestino.Saldo)
+		fmt.Println("Novo saldo da conta Origem:", c.saldo)
+		fmt.Println("Novo saldo da conta Destino:", contaDestino.saldo)
 		return true
 	} else {
 		return false
 	}
+}
+
+func (c *ContaCorrente) ObterSaldo() float64 {
+	return c.saldo
 }
